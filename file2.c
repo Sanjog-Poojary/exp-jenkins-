803a1e735347bde64309aa417bf287401c7e955b
Sanjog-Poojary <2024.sanjog.poojary@ves.ac.in>
2025-09-19 14:59:06 +0530

    Done some formating.

--- file2.c
+++ file2.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
-printf("To test of c codes directly runs on jenkins");
-printf("Yes, the file runs");
+printf("To test of c codes directly runs on jenkins\n");
+printf("Yes, the file runs\n");
 printf("Added a web-hook.");
 return 0;
 }

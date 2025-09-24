cd84c8e36f10d977cd604745fe3c8d3e04d60599
Sanjog-Poojary <2024.sanjog.poojary@ves.ac.in>
2025-09-24 09:50:48 +0530

    Added a line.

--- file1.java
+++ file1.java
@@ -1,7 +1,7 @@
 class file1{
 public static void  main(String args[]){
     System.out.println("Hello world!");
-    System.out.print("This change is made from the local repo.");
+    System.out.println("This change is made from the local repo.");
     System.out.print("This change is made from the local repo.");
    }
 }
